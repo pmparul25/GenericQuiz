883bec1d34f0bb864193c5860ce664babeb4f218
pmparul25 <pmparul25@gmail.com>
2015-12-28 15:12:31 +0530

    Update check_answer.php

--- check_answer.php
+++ check_answer.php
@@ -1,27 +1,16 @@
 <?php
-/*
-**This Code Checks the answer!!
-**MCQ-Single Choice
-**MCQ-Multiple Choice
-***Multiple Choice check consists of +ve as well as -ve marking depending upon number of options selected
-**Text Input Answers
-*/
 include 'config.php';
 session_start();
 $db = $_SESSION['qname'];
 $quizname=$db.'_score';
 //$answer=$_POST['group1'];
-//$ans['answer'] consists of correct answer
 $fetch=mysql_query("SELECT * from $db WHERE q_no='".$_SESSION['q_no']."'");
 $ans=mysql_fetch_array($fetch);
 $result = mysql_query("SELECT * FROM $db");
 $num_rows = mysql_num_rows($result);
 $_SESSION['count']=$num_rows;
-//If Question No. < Last Question No.
-//In order to redirect it to Next Question page
 if($_SESSION['q_no']<$num_rows)
 {
-	//If Answer is MCQ
 	if(!empty($_POST['check_list'])) 
 	{
 		$check = $_POST['check_list'];
@@ -31,10 +20,9 @@ if($_SESSION['q_no']<$num_rows)
 		while($d<=3)
 		{
 			if(isset($check[$d]))
-				$x=$x+1;//Counting value for $x
+				$x=$x+1;
 				$d++;
 		}
-		// Exploding Multiple Answers by ',' in order to check to multiple options
 		$i = substr_count($ans['answer'], ',');
 		$answ = explode(",",$ans['answer']);
 		for($j=0;$j<=$i;$j++)
@@ -51,49 +39,19 @@ if($_SESSION['q_no']<$num_rows)
 				$c=$a/($i+1);		//$c is the Positive Score Scored by the user
 				$y=($x-$a)/($i+1);  //$y is the Negative Score Scored by the user
 				$f=$c-$y;			//$f is the Final Score to be updated
-		$upd=mysql_query("UPDATE $quizname SET score=score+'$f' WHERE email='".$_SESSION['email']."'") or die("Score Not Updated");
+		$t=time();
+		echo($t . "<br>");
+		echo(date("Y-m-d",$t));
+		$upd=mysql_query("UPDATE $quizname SET score=score+'$f', time='$t' WHERE email='".$_SESSION['email']."'") or die("Score Not Updated");
 		$_SESSION['q_no']=$_SESSION['q_no']+1;
 		header('Location: quiz.php?Name='.$db);
 	}
-	//Checking for Simple Text String Answer
-	else
-		if(isset($_POST['answer']))
-		{
-			if($_POST['answer']==$ans['answer'])
-			{
-				$upd=mysql_query("UPDATE $quizname SET score=score+1 WHERE email='".$_SESSION['email']."'") or die("Score Not Updated");
-				$_SESSION['q_no']=$_SESSION['q_no']+1;
-				header('Location: quiz.php?Name='.$db);
-			}
-			else
-			{
-				$_SESSION['q_no']=$_SESSION['q_no']+1;
-				header('Location: quiz.php?Name='.$db);
-			}
-		}
-	else
-		if(isset($_POST['group1']))
-		{
-			if($_POST['group1']==$ans['answer'])
-			{
-				$upd=mysql_query("UPDATE $quizname SET score=score+1 WHERE email='".$_SESSION['email']."'") or die("Score Not Updated");
-				$_SESSION['q_no']=$_SESSION['q_no']+1;
-				header('Location: quiz.php?Name='.$db);
-			}
-			else
-			{
-				$_SESSION['q_no']=$_SESSION['q_no']+1;
-				header('Location: quiz.php?Name='.$db);
-			}
-		}
 	else
 	{
 		$_SESSION['q_no']=$_SESSION['q_no']+1;
 		header('Location: quiz.php?Name='.$db);
 	}
 }
-//When Current Question is Last one of quiz
-//In order to redirect it to result page
 else
 {
 	if(!empty($_POST['check_list'])) 
@@ -124,37 +82,12 @@ else
 				$c=$a/($i+1);		//$c is the Positive Score Scored by the user
 				$y=($x-$a)/($i+1);  //$y is the Negative Score Scored by the user
 				$f=$c-$y;			//$f is the Final Score to be updated
-		$upd=mysql_query("UPDATE $quizname SET score=score+'$f' WHERE email='".$_SESSION['email']."'") or die("Score Not Updated");
+		$t=time();
+		echo($t . "<br>");
+		echo(date("Y-m-d",$t));
+		$upd=mysql_query("UPDATE $quizname SET score=score+'$f' , time='$t' WHERE email='".$_SESSION['email']."'") or die("Score Not Updated");
 		header('Location: result.php?Name='.$db);
 	}
-	else
-		if(isset($_POST['answer']))
-		{
-			if($_POST['answer']==$ans['answer'])
-			{
-				$upd=mysql_query("UPDATE $quizname SET score=score+1 WHERE email='".$_SESSION['email']."'") or die("Score Not Updated");
-				header('Location: result.php?Name='.$db);
-			}
-			else
-			{
-				header('Location: result.php?Name='.$db);
-			}
-		}
-	else
-		if(isset($_POST['group1']))
-		{
-			if($_POST['group1']==$ans['answer'])
-			{
-				$upd=mysql_query("UPDATE $quizname SET score=score+1 WHERE email='".$_SESSION['email']."'") or die("Score Not Updated");
-				$_SESSION['q_no']=$_SESSION['q_no']+1;
-				header('Location: result.php?Name='.$db);
-			}
-			else
-			{
-				$_SESSION['q_no']=$_SESSION['q_no']+1;
-				header('Location: result.php?Name='.$db);
-			}
-		}
 	else
 	{
 		header('Location: result.php?Name='.$db);
